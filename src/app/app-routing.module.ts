26e0bfed27c063e43360618e8fb11680593a6264
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-06-10 10:30:41 -0500

    se acomoda el routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
-import { RegistroComponent } from './pages/registro/registro.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   {path:'home', component: HomeComponent},
-  {path:'product-details', component: ProductDetailsComponent},
-  {path:'register', component: RegistroComponent},
-  {path:'**', redirectTo: 'login'}
+  {path:'**', redirectTo: 'home'}
 ];
 
 @NgModule({
