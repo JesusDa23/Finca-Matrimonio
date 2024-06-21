b42533fd09e6f29b753aa1106a81b0b359e955d1
Julian Leon <julianleon0005@gmail.com>
2024-06-20 19:42:28 -0500

    [+]funcionalidad de botones exitosa

--- src/app/pages/servicios/form-camping/form-camping.component.ts
+++ src/app/pages/servicios/form-camping/form-camping.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './form-camping.component.css'
 })
 export class FormCampingComponent {
+  mostrarMenuActivo: boolean = false;
+  menuVisible: boolean = false;
 
+  mostrarMenu() {
+    this.menuVisible = true;
+    this.mostrarMenuActivo = true;
+  }
+
+  ocultarMenu() {
+    this.menuVisible = false;
+    this.mostrarMenuActivo = false;
+  }
 }
